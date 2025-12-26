0d9178343f9070aaf89972fc02f65f8a5e1f6527
su_wangzk <18576670505@163.com>
2025-12-26 11:49:14 +0800

    remove spa

--- apps/spa/src/contracts/index.ts
+++
@@ -1,8 +0,0 @@
-export { aaveVaultAbi } from './abis/aaveVault';
-export { courseMarketplaceAbi } from './abis/courseMarketplace';
-export { erc20Abi } from './abis/erc20';
-export { mockUSDTAbi } from './abis/mockUsdt';
-export { ydSaleAbi } from './abis/ydSale';
-export { ydTokenAbi } from './abis/ydToken';
-export { ydUsdtSwapAbi } from './abis/ydUsdtSwap';
-export * from './addresses';
--- apps/spa/src/types/index.ts
+++
@@ -1,5 +0,0 @@
-/**
- * 类型定义统一导出
- */
-export type { Course, UICourse } from './course';
-export type { ProfileSource, UserProfile } from './user';
